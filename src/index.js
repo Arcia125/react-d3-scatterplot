17cf4f6bac2e93d8e771c47f4de06249a5e2959e
Arcia125 <tauren125@gmail.com>
2017-03-03 22:39:16 -0600

    create initial graph

--- src/index.js
+++ src/index.js
@@ -22,40 +22,39 @@ const getData = () => new Promise((resolve, reject) => {
 });
 
 getData()
-    .then((jsonData) => {
-        const scaleAmount = 30;
-        const scaleDown = (h) => h / scaleAmount;
+    .then((data) => {
+        // const scaleAmount = 30;
+        // const scaleDown = (h) => h / scaleAmount;
 
-        const data = jsonData.data;
-
-        const maxY = d3.max(data, d => d[1]);
+        const maxY = d3.max(data, d => d.Place);
         const width = 1000;
-        const height = scaleDown(maxY);
+        const height = 600;
+
+        const fastestTime = d3.min(data, d => d.Seconds);
+        const slowestTime = d3.max(data, d => d.Seconds);
 
         const dataLength = data.length;
         const barWidth = width / dataLength;
 
-        const minDate = new Date(data[0][0]);
-        const maxDate = new Date(data[dataLength - 1][0]);
 
         const rectFillNormal = `#5F5FFF`;
         const rectFillHover = `#9F9FFF`;
 
-        const x = d3.scaleTime()
-            .domain([minDate, maxDate])
+        const xScale = d3.scaleLinear()
+            .domain([60 * 3.5, 0])
             .range([0, width]);
 
-        const y = d3.scaleLinear()
-            .range([height, 0])
-            .domain([0, maxY]);
+        const yScale = d3.scaleLinear()
+            .domain([1, dataLength])
+            .range([0, height]);
 
-        const xAxis = d3.axisBottom(x)
-            .ticks(d3.timeYear.every(5));
-        const yAxis = d3.axisLeft(y)
-            .ticks(10);
+        const xAxis = d3.axisBottom(xScale)
+            .ticks(d3.timeSecond.every(30));
+        const yAxis = d3.axisLeft(yScale)
+            .ticks(6);
 
         const container = d3.select(`.container`)
-            .style(`width`, `${width + 200}px`)
+            .style(`width`, `${width}px`)
             .style(`height`, `${height}px`)
             .style(`margin`, `auto`);
 
@@ -80,47 +79,72 @@ getData()
         const title = chart.append(`text`)
             .attr(`font-size`, `2.5em`)
             .attr(`transform`, `translate(${width / 3.5}, 0)`)
-            .text(`Gross Domestic Product`);
+            .text(`Doping in Professional Bicycle Racing`);
 
+        const points = chart.selectAll(`circle`)
+            .data(data)
+            .enter()
+            .append(`circle`)
+            .attr(`cx`, d => xScale(d.Seconds - fastestTime))
+            .attr(`cy`, d => yScale(d.Place))
+            .attr(`r`, 5)
+            .attr(`fill`, d => d.Doping === `` ? `#000` : `#F00`)
+            .attr(`data-legend`, d => d.Doping === `` ? `No Doping Allegation` : `Doping Allegations`)
+            .on(`mouseover`, (d) => {
+                tooltip.transition()
+                    .duration(200)
+                    .style(`opacity`, .9);
+                tooltip.html(`<span>Place: ${d.Place}</span>`)
+            })
+            .on(`mouseout`, (d) => {
+                tooltip.transition()
+                    .duration(500)
+                    .style(`opacity`, 0);
+            });
 
-        const bars = chart.selectAll(`g`)
-                .data(data)
-            .enter().append(`g`)
-                .attr(`transform`, (d, i) => `translate(${i * barWidth}, ${height - scaleDown(d[1])})`)
-            .append(`rect`)
-                .attr(`width`, barWidth)
-                .attr(`fill`, rectFillNormal)
-                .attr(`height`, d => scaleDown(d[1]))
-                .on(`mouseover`, function (d) {
-                    const rect = d3.select(this);
-                    rect.attr(`fill`, rectFillHover);
-                    tooltip.transition()
-                        .duration(250)
-                        .style(`opacity`, `1`);
-                    tooltip.html(`<span style='font-weight: bold'>${d3.format(`$,.2f`)(d[1])} Billion</span><br><span>${d3.timeFormat(`%B - %Y`)(new Date(d[0]))}</span>`)
-                        .style(`left`, `${d3.event.pageX + 10}px`)
-                        .style(`top`, `${d3.event.pageY - 30}px`);
-                })
-                .on(`mouseleave`, function (d) {
-                    const rect = d3.select(this);
-                    rect.attr(`fill`, rectFillNormal);
-                    tooltip.transition()
-                        .duration(500)
-                        .style(`opacity`, `0`);
-                })
+        const pointText = chart.selectAll(`text`)
+            .data(data)
+            .enter()
+            .append(`text`)
+            .text(d => d.Name)
+            .attr(`x`, d => xScale(d.Seconds - fastestTime))
+            .attr(`y`, d => yScale(d.Place))
+            .attr(`transform`, `translate(15, +4)`);
 
-        const xTicks = chart.append(`g`)
-            .attr(`class`, `x axis`)
-            .attr(`transform`, `translate(0, ${height})`)
-            .call(xAxis);
+        // const bars = chart.selectAll(`g`)
+        //         .data(data)
+        //     .enter().append(`g`)
+        //         .attr(`transform`, (d, i) => `translate(${i * barWidth}, ${height - scaleDown(d[1])})`)
+        //     .append(`rect`)
+        //         .attr(`width`, barWidth)
+        //         .attr(`fill`, rectFillNormal)
+        //         .attr(`height`, d => scaleDown(d[1]))
+        //         .on(`mouseover`, function (d) {
+        //             const rect = d3.select(this);
+        //             rect.attr(`fill`, rectFillHover);
+        //             tooltip.transition()
+        //                 .duration(250)
+        //                 .style(`opacity`, `1`);
+        //             tooltip.html(`<span style='font-weight: bold'>${d3.format(`$,.2f`)(d[1])} Billion</span><br><span>${d3.timeFormat(`%B - %Y`)(new Date(d[0]))}</span>`)
+        //                 .style(`left`, `${d3.event.pageX + 10}px`)
+        //                 .style(`top`, `${d3.event.pageY - 30}px`);
+        //         })
+        //         .on(`mouseleave`, function (d) {
+        //             const rect = d3.select(this);
+        //             rect.attr(`fill`, rectFillNormal);
+        //             tooltip.transition()
+        //                 .duration(500)
+        //                 .style(`opacity`, `0`);
+        //         })
 
-        const yTicks = chart.append(`g`)
-            .attr(`class`, `y axis`)
-            .call(yAxis);
+        // const xTicks = chart.append(`g`)
+        //     .attr(`class`, `x axis`)
+        //     .attr(`transform`, `translate(0, ${height})`)
+        //     .call(xAxis);
 
-        const notes = chart.append(`text`)
-            .attr(`transform`, `translate(0, ${height * 1.1})`)
-            .attr(`font-size`, `11px`)
-            .text(jsonData.description);
+        // const yTicks = chart.append(`g`)
+        //     .attr(`class`, `y axis`)
+        //     .call(yAxis);
+
     })
     .catch(err => console.log(err));
